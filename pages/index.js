40dfb33224d6f2ebd72e80438864bf2684d05aaf
cookie777 <l.oeil.ballon@gmail.com>
2021-01-02 21:41:41 -0800

    test:

--- pages/index.js
+++ pages/index.js
@@ -3,66 +3,58 @@ import Head from "next/head";
 import matter from "gray-matter";
 import Link from "next/link";
 
-const Index = () =>{
+
+const Index = ({ data, title, description }) => {
+  const ListItems = data.map((blog) => matter(blog).data);
+
+
   return (
-    <div>
-      aaaaa
-    </div>
-  )
-}
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="Description" content={description}></meta>
+        <title>{title}</title>
+      </Head>
+      <h1>My First Blog ✍ </h1>
+      <div>
+        <ul>
+          {ListItems.map((blog, i) => (
+            <li key={i}>
+              <Link href={`/post/${blog.slug}`}>
+                <a>{blog.title}</a>
+              </Link>
+                <p>{blog.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
 export default Index;
 
-// const Index = ({ data, title, description }) => {
-//   const ListItems = data.map((blog) => matter(blog).data);
 
+export async function getStaticProps() {
+  const siteData = await import(`../config.json`);
 
-//   return (
-//     <>
-//       <Head>
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <meta charSet="utf-8" />
-//         <meta name="Description" content={description}></meta>
-//         <title>{title}</title>
-//       </Head>
-//       <h1>My First Blog ✍ </h1>
-//       <div>
-//         <ul>
-//           {ListItems.map((blog, i) => (
-//             <li key={i}>
-//               <Link href={`/post/${blog.slug}`}>
-//                 <a>{blog.title}</a>
-//               </Link>
-//                 <p>{blog.description}</p>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// };
+  const fs = require("fs");
+  const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
+  const blogs = files.filter((fn) => fn.endsWith(".md") && !fn.match("undefined.md") );
+  const data = blogs.map((blog) => {
+    const path = `${process.cwd()}/content/${blog}`;
+    const rawContent = fs.readFileSync(path, {
+      encoding: "utf-8",
+    });
+    return rawContent;
+  });
 
-// export default Index;
-
-
-// export async function getStaticProps() {
-//   const siteData = await import(`../config.json`);
-
-//   const fs = require("fs");
-//   const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
-//   const blogs = files.filter((fn) => fn.endsWith(".md") && !fn.match("undefined.md") );
-//   const data = blogs.map((blog) => {
-//     const path = `${process.cwd()}/content/${blog}`;
-//     const rawContent = fs.readFileSync(path, {
-//       encoding: "utf-8",
-//     });
-//     return rawContent;
-//   });
-
-//   return {
-//     props: {
-//       data: data,
-//       title: siteData.default.title,
-//       description: siteData.default.description,
-//     },
-//   };
-// }
+  return {
+    props: {
+      data: data,
+      title: siteData.default.title,
+      description: siteData.default.description,
+    },
+  };
+}
